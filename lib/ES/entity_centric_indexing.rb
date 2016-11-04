ff45a2d9dd3744b659d3cd6d3efb3ab223deac26
Jonny Pillar <info@jonnypillar.co.uk>
2016-11-04 10:00:45 +0000

    Reworked entity_centric_indexing to work with ES 2.3

--- lib/ES/entity_centric_indexing.rb
+++ lib/ES/entity_centric_indexing.rb
@@ -4,69 +4,101 @@ require 'json'
 module ES
   class EntityCentricIndexing
     def initialize(args)
-      @entityIdField = args.entityIdField
-      @eventIndexName = args.eventIndexName
-      @eventDocType = args.eventDocType
-      @eventsPerScrollPage = args.eventsPerScrollPage
-      @maxTimeToProcessScrollPage = args.maxTimeToProcessScrollPage
+      @entityIdField = '_id'
+      @eventIndexName = 'reviews'
+      @eventDocType = 'review'
+      @eventsPerScrollPage = 5000
+      @maxTimeToProcessScrollPage = '1m'
       @preserve_order = true;
-      @event_query_file = args.eventQueryFile
-      @update_script_file = args.updateScriptFile
-      @script_mode = args.updateScriptFile
+      # @event_query_file = args.eventQueryFile
+      @update_script_file = 'ReviewerProfileUpdater'
+      @script_mode = "fullBuild"
+
+      # @entityIdField = args.entityIdField
+      # @eventIndexName = args.eventIndexName
+      # @eventDocType = args.eventDocType
+      # @eventsPerScrollPage = args.eventsPerScrollPage
+      # @maxTimeToProcessScrollPage = args.maxTimeToProcessScrollPage
+      # @preserve_order = true;
+      # @event_query_file = args.eventQueryFile
+      # @update_script_file = args.updateScriptFile
+      # @script_mode = args.updateScriptFile
     end
 
     def generate_actions
-      last_entity_id = ''
+      last_reviewer_id = ''
       events = []
+      actions = []
       numDocsProcessed = 0
 
       ES::Scan.scan(
         client,
         index: @eventIndexName,
-        doc_type: eventDocType,
+        doc_type: @eventDocType,
         query: events_query,
-        size: eventsPerScrollPage,
-        scroll: maxTimeToProcessScrollPage,
-        preserve_order: preserve_order
-      ) do
-        thisEntityId = doc["_source"][@entityIdField]
-
-        if thisEntityId != last_entity_id
-          if events != nil
-            get_action(events, last_entity)
+        size: @eventsPerScrollPage,
+        scroll: @maxTimeToProcessScrollPage,
+        preserve_order: @preserve_order
+      ) do |doc|
+        doc.map do |item|
+          reviewerId = item['_source']['reviewerId']
+          if reviewerId != last_reviewer_id
+            if events != nil
+              actions.push(get_action(events, last_reviewer_id))
+            end
+            events = []
+            last_reviewer_id = reviewerId
           end
-          events = []
-          last_entity_id = thisEntityId
+
+          events.push(item["_source"])
+          numDocsProcessed += 1
+          puts "Processed #{numDocsProcessed} docs"
         end
-        events.push(doc["_source"])
-        numDocsProcessed += 1
+
       end
-      puts "Processed", numDocsProcessed, "docs"
+      puts "Complete Processing #{numDocsProcessed} docs"
+
+      actions.each_slice(5000) do |batch|
+        client.bulk body: batch, index: 'profiles', type: 'profile'
+        puts "Completed another bulk batch"
+      end
     end
 
-    def get_action(events, last_entity_id)
+    def get_action(events, last_reviewer_id)
       {
-        _op_type: 'update',
-        _id: last_entity_id,
-        scripted_upsert: true,
-        # In elasticsearch >=2.0
-        script: {
-          file: @update_script_file,
-          params: {
-            scriptMode: @script_mode,
-            events: events
+        update: {
+          _index: 'profiles',
+          _type: 'profile',
+          _id: last_reviewer_id,
+          data: {
+            script: {
+              # _id: last_reviewer_id,
+              file: @update_script_file,
+              params: {
+                scriptMode: @script_mode,
+                events: events
+              }
+            },
+            scripted_upsert: true,
+            upsert: {}
           }
-        },
-        # In elasticsearch <2.0
-        # "script": args.update_script_file,
-        # "params": {
-        #     "scriptMode": args.scriptMode,
-        #     "events":list(events)
-        # },
-        upsert: {
-          # Use a blank document because script does all the initialization
         }
       }
+
+      # {
+      #   script: {
+      #     # _id: last_reviewer_id,
+      #     id: @update_script_file,
+      #     params: {
+      #       scriptMode: @script_mode,
+      #       events: events
+      #     }
+      #   },
+      #   scripted_upsert: true,
+      #   upsert: {
+      #     # Use a blank document because script does all the initialization
+      #   }
+      # }
     end
 
     private
@@ -76,7 +108,31 @@ module ES
     end
 
     def events_query
-      @events_query ||= json.load(open(@event_query_file))
+      {
+        "query": {
+          "bool": {
+            "mustNot": [
+              {
+                "term": {
+                  'rating': 0
+                }
+              }
+            ]
+          }
+        },
+        "sort": [
+          {
+            "reviewerId": {
+              "order": 'asc'
+            }
+          },
+          {
+            "date": {
+              "order": 'asc'
+            }
+          }
+        ]
+      }
     end
   end
 end
--- lib/ES/scan.rb
+++ lib/ES/scan.rb
@@ -1,10 +1,18 @@
 module ES
   class Scan
     def self.scan(client, args, &block)
-      search = client.search index: args[:index], search_type: 'scan', scroll: '5m', size: args[:size]
+      search = client.search(
+        index: args[:index],
+        # doc_type: args[:doc_type],
+        body: args[:query],
+        search_type: 'scan',
+        scroll: '5m',
+        size: args[:size],
+        # preserve_order: args[:preserve_order]
+      )
 
       while search = client.scroll(scroll_id: search['_scroll_id'], scroll: '5m') and not search['hits']['hits'].empty? do
-        yield r['hits']['hits']
+        yield search['hits']['hits']
       end
     end
   end
